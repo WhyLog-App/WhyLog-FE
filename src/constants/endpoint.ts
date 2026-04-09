fd8550ee5c01d4d873154c2eb2c48e004091b66e
hyeseong <tell280210@gmail.com>
2026-04-09 13:47:30 +0900

    feat: ROUTES 상수 업데이트

--- src/constants/endpoint.ts
+++ src/constants/endpoint.ts
@@ -26,8 +26,11 @@ export const ROUTES = {
   APP_ROOT: "/",
   SIGNUP: "/signup",
   LOGIN: "/login",
+  DECISIONS: "/decisions",
   MEETING: "/meeting",
   MEETING_DETAIL: "/meeting/:meetingId",
+  GIT: "/git",
+  SETTINGS: "/settings",
 } as const;
 
 export default ENDPOINT;
