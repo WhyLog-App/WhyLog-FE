50690a4f0b8f22de64d1d6a3e0fc42008f72996b
hyeseong <tell280210@gmail.com>
2026-05-04 17:53:59 +0900

    fix: 홈 및 설정 화면에서 패널 숨기도록 수정

--- src/components/panel/index.tsx
+++ src/components/panel/index.tsx
@@ -1,3 +1,4 @@
+import type { ReactNode } from "react";
 import { useLocation } from "react-router-dom";
 import DecisionPanel from "./decision/DecisionPanel";
 import MeetingPanel from "./meeting/MeetingPanel";
@@ -6,22 +7,20 @@ import RepositoryPanel from "./repository/RepositoryPanel";
 const Panel = () => {
   const { pathname } = useLocation();
 
-  const renderContent = () => {
-    if (/\/decisions(\/|$)/.test(pathname)) {
-      return <DecisionPanel />;
-    }
-    if (/\/meeting(\/|$)/.test(pathname)) {
-      return <MeetingPanel />;
-    }
-    if (/\/git(\/|$)/.test(pathname)) {
-      return <RepositoryPanel />;
-    }
-    return null;
-  };
+  let content: ReactNode = null;
+  if (/\/decisions(\/|$)/.test(pathname)) {
+    content = <DecisionPanel />;
+  } else if (/\/meeting(\/|$)/.test(pathname)) {
+    content = <MeetingPanel />;
+  } else if (/\/git(\/|$)/.test(pathname)) {
+    content = <RepositoryPanel />;
+  }
+
+  if (!content) return null;
 
   return (
     <aside className="flex h-screen w-70 shrink-0 flex-col gap-4 overflow-hidden border-r border-solid border-(--color-border-default) bg-(--color-bg-surface) pt-10 pb-8 shadow-[0px_0px_2px_0px_rgba(40,41,61,0.04),0px_4px_8px_0px_rgba(96,97,112,0.16)]">
-      {renderContent()}
+      {content}
     </aside>
   );
 };
